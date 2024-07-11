68d9aa1114dd4171abb68f36ffb1a802d146f17c
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-11 13:11:57 -0500

    feat(home): add carousel to main section 🎠

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,30 @@
+import dynamic from "next/dynamic";
 import { useTranslations } from "next-intl";
+import "./styles.module.css";
+
+const Sc1_Name = dynamic(() => import("@/components/main/section_1/name"));
+const Sc1_Carousel = dynamic(() => import("@/components/main/section_1/carousel"), { ssr: false });
 
 export default function Home() {
+	const t = useTranslations("Main.Sections");
+
 	return (
-		<></>
+		<>
+		{/* Main - #1 */}
+		<section className="w-full h-[100vh]">
+			<div className="container h-full flex flex-col items-center justify-center">
+				<Sc1_Name />
+				<Sc1_Carousel />
+			</div>
+		</section>
+
+		{/* About Me - #2 */}
+		<section className="w-full h-[100vh]">
+		</section>
+
+		{/* Hard Skills */}
+		<section className="w-full h-[100vh]">
+		</section>
+		</>
 	);
 }
---
+++ src/components/main/section_1/carousel.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { v4 as uuidv4 } from "uuid";
+import { useTranslations } from "next-intl";
+import { useEffect, useMemo, useState, useCallback } from "react";
+
+import style from "./styles.module.css";
+
+export default function Carousel() {
+	const t = useTranslations("Main.Sections.Main");
+	const text = useMemo(() => t("Carousel").split(","), [t]);
+	const changeInterval = 5 * 1000;
+
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [prevIndex, setPrevIndex] = useState(text.length - 1);
+
+	const updateIndex = useCallback(() => {
+		setPrevIndex(currentIndex);
+		setCurrentIndex((prevIndex) => (prevIndex + 1) % text.length);
+	}, [currentIndex, text.length]);
+
+	useEffect(() => {
+		const intervalId = setInterval(updateIndex, changeInterval);
+
+		return () => clearInterval(intervalId);
+	}, [updateIndex, changeInterval]);
+
+	return (
+		<div className="w-full overflow-hidden">
+			<div className="relative h-[4rem] md:h-[8rem]">
+				{text.map((item, index) => (
+					<div key={uuidv4()} className={`absolute w-full h-full flex justify-center items-center opacity-0 ${index === currentIndex ? style["slide-in"] : index === prevIndex ? style["slide-out"] : ""}`}>
+						<h2 className={`text-center leading-none text-[2rem] md:text-[4rem] xl:text-[6rem] font-bold bg-gradient-to-b from-indigo-400 to-cyan-400 bg-clip-text text-transparent py-[1rem]`}>{item}</h2>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
