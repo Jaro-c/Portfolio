aa7ff62daeb1ba9108338072997d7425d0c3ed7b
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-11 18:05:31 -0500

    feat(home): add bouncing double arrow to bottom 🔽

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,15 +2,23 @@ import dynamic from "next/dynamic";
 
 const Sc1_Name = dynamic(() => import("@/components/main/section_1/name"));
 const Sc1_Carousel = dynamic(() => import("@/components/main/section_1/carousel"), { ssr: false });
+const Sc1_More = dynamic(() => import("@/components/main/section_1/more"));
 
 export default function Home() {
 	return (
 		<div className="custom-scrollbar snap-y snap-mandatory h-screen overflow-y-auto">
 			{/* Main - #1 */}
-			<section className="w-full h-full snap-start snap-always">
-				<div className="container h-full flex flex-col items-center justify-center">
+			<section className="w-full h-full snap-start snap-always flex flex-col items-center">
+				<div className="container h-full flex flex-col items-center justify-center space-y-[2rem]">
 					<Sc1_Name />
 					<Sc1_Carousel />
+					<Sc1_More />
+				</div>
+
+				<div className="mb-[4rem] animate-bounce">
+					<svg className="rotate-90 fill-gray-500 w-[1rem] h-[1rem] md:w-[2rem] md:h-[2rem]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+						<path d="M0 3.795l2.995-2.98 11.132 11.185-11.132 11.186-2.995-2.981 8.167-8.205-8.167-8.205zm18.04 8.205l-8.167 8.205 2.995 2.98 11.132-11.185-11.132-11.186-2.995 2.98 8.167 8.206z"/>
+					</svg>
 				</div>
 			</section>
 
