c5aa54506130e1d555d9449255537489b2a499f6
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-11 15:20:05 -0500

    feat(home): add scroll sections 🖱️

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,30 +1,30 @@
 import dynamic from "next/dynamic";
-import { useTranslations } from "next-intl";
-import "./styles.module.css";
 
 const Sc1_Name = dynamic(() => import("@/components/main/section_1/name"));
 const Sc1_Carousel = dynamic(() => import("@/components/main/section_1/carousel"), { ssr: false });
 
 export default function Home() {
-	const t = useTranslations("Main.Sections");
-
 	return (
-		<>
-		{/* Main - #1 */}
-		<section className="w-full h-[100vh]">
-			<div className="container h-full flex flex-col items-center justify-center">
-				<Sc1_Name />
-				<Sc1_Carousel />
-			</div>
-		</section>
+		<div className="custom-scrollbar snap-y snap-mandatory h-screen overflow-y-auto">
+			{/* Main - #1 */}
+			<section className="w-full h-full snap-start snap-always">
+				<div className="container h-full flex flex-col items-center justify-center">
+					<Sc1_Name />
+					<Sc1_Carousel />
+				</div>
+			</section>
 
-		{/* About Me - #2 */}
-		<section className="w-full h-[100vh]">
-		</section>
+			{/* About Me - #2 */}
+			<section className="w-full h-full snap-start snap-always">
+			</section>
 
-		{/* Hard Skills */}
-		<section className="w-full h-[100vh]">
-		</section>
-		</>
+			{/* Hard Skills - #3 */}
+			<section className="w-full h-full snap-start snap-always">
+			</section>
+
+			{/* Contact - #4 */}
+			<section className="w-full h-full snap-start snap-always">
+			</section>
+		</div>
 	);
 }
