e93f0b735339638328542306d6c6de3b70ec1796
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-12 12:40:16 -0500

    feat(about): add new about section 🆕

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 import dynamic from "next/dynamic";
+import { useTranslations } from "next-intl";
+
+const SectionTitle = dynamic(() => import("@/components/main/section_title"));
 
 const Sc1_Name = dynamic(() => import("@/components/main/section_1/name"));
 const Sc1_Carousel = dynamic(() => import("@/components/main/section_1/carousel"), { ssr: false });
 const Sc1_More = dynamic(() => import("@/components/main/section_1/more"));
 
 export default function Home() {
+	const t = useTranslations("Main.Sections");
+
 	return (
 		<div className="custom-scrollbar snap-y snap-mandatory h-screen overflow-y-auto scroll-smooth">
 			{/* Main - #1 */}
@@ -24,7 +30,20 @@ export default function Home() {
 			</section>
 
 			{/* About Me - #2 */}
-			<section id="about" className="w-full h-full snap-start snap-always">
+			<section id="about" className="w-full h-full snap-start snap-always flex flex-col justify-center items-center space-y-[4rem]">
+				<SectionTitle title={t("About.title")} />
+
+				<div className="container flex flex-col xl:flex-row items-center justify-evenly lg:space-y-[2rem] xl:space-y-0">
+					<div className="hidden lg:block">
+						<Image className="rounded-full shadow-[0px_0px_20px_1px_#645180]" src="/me.webp" width={300} height={300} alt="Jaro.c | Alejandro" />
+					</div>
+
+					<p className="lg:w-4/5 xl:w-3/5 2xl:w-2/5 text-secondary text-justify opacity-80 whitespace-pre-line">
+						{t.rich("About.description", {
+							name: (chunks) => <span className="font-semibold underline decoration-2">{chunks}</span>
+						})}
+					</p>
+				</div>
 			</section>
 
 			{/* Hard Skills - #3 */}
---
+++ src/components/main/section_title.tsx
@@ -0,0 +1,7 @@
+interface TitleProps {
+	title: string;
+}
+
+export default function SectionTitle({ title }: TitleProps) {
+	return <h2 className="text-[2rem] md:text-[4rem] xl:text-[6rem] font-bold bg-gradient-to-b from-gray-50 to-gray-700 bg-clip-text text-transparent">{title}</h2>;
+}
