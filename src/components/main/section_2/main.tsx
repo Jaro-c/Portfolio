938d5659fc574f8728797d67baadddb7c0ea43fa
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-13 22:21:00 -0500

    feat(about): add no-select CSS class to about me image 🖼️

--- src/components/main/section_2/main.tsx
+++ src/components/main/section_2/main.tsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
 	return (
 		<div className="container flex flex-col xl:flex-row items-center justify-evenly lg:space-y-[2rem] xl:space-y-0">
 			<div className="hidden lg:block">
-				<Image className="rounded-full shadow-[0px_0px_20px_1px_#645180]" src="/me.webp" width={300} height={300} alt="Jaro.c | Alejandro" />
+				<Image className="no-select rounded-full shadow-[0px_0px_20px_1px_#645180]" src="/me.webp" width={300} height={300} alt="Jaro.c | Alejandro" />
 			</div>
 
 			<p className="lg:w-4/5 xl:w-3/5 2xl:w-2/5 text-secondary text-justify opacity-80 whitespace-pre-line">
