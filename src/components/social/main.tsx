900a6529e16e5d8ce4a33d3a51694db05817085e
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-24 10:01:41 -0500

    📏 style: change width and height for lg resolution

--- src/components/social/main.tsx
+++ src/components/social/main.tsx
@@ -18,7 +18,7 @@ export default async function Social_Network() {
 				{socials.filter((social) => social.icon && social.name && social.url).map((social) => (
 					<li key={uuidv4()} className="animate-pulse hover:animate-none">
 						<a className="inline-block rounded-2xl border-2 border-gray-700 border-r-gray-500 border-b-gray-500 transition-all duration-200 ease-in group hover:shadow-[0px_0px_15px_1px_#fff]" href={social.url} target="_blank" rel="noopener noreferrer" title={social.name}>
-							<svg className="fill-gray-600 transition-all duration-200 ease-in group-hover:fill-white m-[1rem] w-[1rem] h-[1rem] lg:w-[2rem] lg:h-[2rem]" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d={social.icon}></path></svg>
+							<svg className="fill-gray-600 transition-all duration-200 ease-in group-hover:fill-white m-[1rem] w-[1rem] h-[1rem] lg:w-[1.5rem] lg:h-[1.5rem]" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d={social.icon}></path></svg>
 						</a>
 					</li>
 				))}
