a2ea9dbdc41250f4249c4b82c366cdc4da09e2b8
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-24 10:18:35 -0500

    🧑‍🎨 style: remove select

--- src/components/main/section_3/skills.tsx
+++ src/components/main/section_3/skills.tsx
@@ -12,9 +12,9 @@ interface Skills_Props {
 
 export default function Skills({ skills }: Skills_Props) {
 	return (
-		<div className="container flex flex-wrap items-center justify-center space-x-[.4rem]">
+		<div className="container no-select flex flex-wrap items-center justify-center space-x-[.4rem]">
 			{skills.map((skill) => (
-				<div key={uuidv4()} className="no-select flex items-center justify-center space-x-[.4rem] px-[.8rem] py-[.5rem] mb-[.4rem] rounded-lg bg-gray-900 group transition-colors duration-300 ease-in-out hover:bg-gray-50">
+				<div key={uuidv4()} className="flex items-center justify-center space-x-[.4rem] px-[.8rem] py-[.5rem] mb-[.4rem] rounded-lg bg-gray-900 group transition-colors duration-300 ease-in-out hover:bg-gray-50">
 					<Image className="max-h-5" src={skill.url} width={20} height={20} alt={skill.name} />
 					<span className="text-[.8rem] lg:text-[1rem] text-secondary opacity-80 transition-colors duration-300 ease-in-out group-hover:opacity-100 group-hover:text-black">{skill.name}</span>
 				</div>
--- src/components/main/section_3/title.tsx
+++ src/components/main/section_3/title.tsx
@@ -4,6 +4,6 @@ interface TitleProps {
 
 export default function Skill_Title({ title }: TitleProps) {
 	return (
-		<h3 className="text-center text-[1rem] lg:text-[1.5rem] bg-gradient-to-b from-gray-50 to-gray-500 bg-clip-text text-transparent">{title}</h3>
+		<h3 className="no-select text-center text-[1rem] lg:text-[1.5rem] bg-gradient-to-b from-gray-50 to-gray-500 bg-clip-text text-transparent">{title}</h3>
 	);
 }
