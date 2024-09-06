c1abc7b56bc3b3522b2ca49b705d0dae7f2c50e5
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-09-06 13:45:45 -0500

    🎓 feat: add NestJS skill

--- src/components/main/section_3/skills.tsx
+++ src/components/main/section_3/skills.tsx
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 interface Skill_Props {
 	name: string;
-	url: string;
+	icon: string;
 }
 
 interface Skills_Props {
@@ -15,7 +15,7 @@ export default function Skills({ skills }: Skills_Props) {
 		<div className="container no-select flex flex-wrap items-center justify-center space-x-[.4rem]">
 			{skills.map((skill) => (
 				<div key={uuidv4()} className="flex items-center justify-center space-x-[.4rem] px-[.8rem] py-[.5rem] mb-[.4rem] rounded-lg bg-gray-900 group transition-colors duration-300 ease-in-out hover:bg-gray-50">
-					<Image className="max-h-5" src={skill.url} width={20} height={20} alt={skill.name} />
+					<Image className="max-h-5" src={skill.icon} width={20} height={20} alt={skill.name} />
 					<span className="text-[.8rem] lg:text-[1rem] text-secondary opacity-80 transition-colors duration-300 ease-in-out group-hover:opacity-100 group-hover:text-black">{skill.name}</span>
 				</div>
 			))}
