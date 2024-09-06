b16d54fba26d7687a4c0cb05b0801ce9c4a4944b
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-09-06 16:29:11 -0500

    🔧 chore: add `noindex` to "Not Found" page

--- src/app/[locale]/[...not-found]/page.tsx
+++ src/app/[locale]/[...not-found]/page.tsx
@@ -5,6 +5,11 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
 	title: "404",
 	alternates: { canonical: "404" },
+	robots: {
+		index: false,
+		follow: false,
+		nocache: false,
+	},
 };
 
 export default function NotFound() {
