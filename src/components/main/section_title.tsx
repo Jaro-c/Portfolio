6aebb247294841f3e6a851470927292697585545
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-09-06 15:38:55 -0500

    💄 style: change title size

--- src/components/main/section_title.tsx
+++ src/components/main/section_title.tsx
@@ -3,5 +3,9 @@ interface TitleProps {
 }
 
 export default function SectionTitle({ title }: TitleProps) {
-	return <h2 className="select-none text-[2rem] md:text-[4rem] xl:text-[6rem] font-bold bg-gradient-to-b from-gray-50 to-gray-800 bg-clip-text text-transparent">{title}</h2>;
+	return (
+		<h2 className="select-none text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[4rem] 2xl:text-[5rem] 3xl:text-[6rem] 4xl:text-[7rem] font-bold bg-gradient-to-b from-gray-50 to-gray-800 bg-clip-text text-transparent">
+			{title}
+		</h2>
+	);
 }
