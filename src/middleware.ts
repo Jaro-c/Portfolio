e80382f2bf3a63ba2ed5084d97790acb9b567945
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-09-06 15:08:19 -0500

    🐛 Fix: Handle malformed URI error in middleware and improve URL validation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import createMiddleware from "next-intl/middleware";
 
 const intlMiddleware = createMiddleware({
@@ -34,22 +34,26 @@ export default async function middleware(request: NextRequest) {
 		upgrade-insecure-requests;`;
 
 	/* Languages */
-	const response = intlMiddleware(request);
-
-	response.headers.set("Content-Security-Policy", cspHeader.replace(/\s{2,}/g, " ").trim());
-	response.headers.set("X-Content-Type-Options", "nosniff");
-	response.headers.set("X-Frame-Options", "DENY");
-	response.headers.set("X-XSS-Protection", "1; mode=block");
-	response.headers.set("Referrer-Policy", "no-referrer");
-
-	response.headers.set("Cross-Origin-Resource-Policy", "same-origin");
-	response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
-	response.headers.set("X-Download-Options", "noopen");
-	response.headers.set("X-Permitted-Cross-Domain-Policies", "none");
-	response.headers.set("Origin-Agent-Cluster", "?1");
-	response.headers.set("Permissions-Policy", "camera=(), display-capture=(), fullscreen=(), geolocation=(), microphone=()");
-
-	return response;
+	try {
+		const response = intlMiddleware(request);
+	
+		response.headers.set("Content-Security-Policy", cspHeader.replace(/\s{2,}/g, " ").trim());
+		response.headers.set("X-Content-Type-Options", "nosniff");
+		response.headers.set("X-Frame-Options", "DENY");
+		response.headers.set("X-XSS-Protection", "1; mode=block");
+		response.headers.set("Referrer-Policy", "no-referrer");
+	
+		response.headers.set("Cross-Origin-Resource-Policy", "same-origin");
+		response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
+		response.headers.set("X-Download-Options", "noopen");
+		response.headers.set("X-Permitted-Cross-Domain-Policies", "none");
+		response.headers.set("Origin-Agent-Cluster", "?1");
+		response.headers.set("Permissions-Policy", "camera=(), display-capture=(), fullscreen=(), geolocation=(), microphone=()");
+	
+		return response;
+	} catch (error) {
+		return NextResponse.redirect(new URL("/404", request.url));
+	}
 }
 
 export const config = {
