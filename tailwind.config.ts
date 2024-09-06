2eaec3f7881c81f6d2acdecb6e8da7d91dd24067
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-09-06 13:49:08 -0500

    🐛 fix: resolve issue with Tailwind CSS max-sm not working

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,11 +13,6 @@ const config: Config = {
 				secondary: "#E3E9FF"
 			},
 			screens: {
-				sm: "100%",
-				md: "768px",
-				lg: "1024px",
-				xl: "1280px",
-				"2xl": "1536px",
 				"3xl": "1920px",
 				"4xl": "2560px"
 			},
